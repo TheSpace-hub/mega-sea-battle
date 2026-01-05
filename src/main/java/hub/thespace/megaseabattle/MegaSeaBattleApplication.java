34a4a0a73215dc637d5dbcdc81acbbde040b08e9
TheSpace-hub <biometer2020@gmail.com>
2026-01-05 23:58:47 +0300

    Initial commit

---
+++ src/main/java/hub/thespace/megaseabattle/MegaSeaBattleApplication.java
@@ -0,0 +1,13 @@
+package hub.thespace.megaseabattle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegaSeaBattleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MegaSeaBattleApplication.class, args);
+    }
+
+}
