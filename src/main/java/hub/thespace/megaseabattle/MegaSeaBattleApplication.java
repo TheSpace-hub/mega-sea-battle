6778e0c0806c012f2968e958d5e7273472d21a30
TheSpace-hub <biometer2020@gmail.com>
2026-01-06 20:56:40 +0300

    Remove default games

--- src/main/java/hub/thespace/megaseabattle/MegaSeaBattleApplication.java
+++ src/main/java/hub/thespace/megaseabattle/MegaSeaBattleApplication.java
@@ -1,6 +1,5 @@
 package hub.thespace.megaseabattle;
 
-import hub.thespace.megaseabattle.game.GameConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,9 +8,6 @@ public class MegaSeaBattleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MegaSeaBattleApplication.class, args);
-
-        GameConfig.games.add(new GameConfig("qwert", "Морской Волк", 3));
-        GameConfig.games.add(new GameConfig("trewq", "Серёга Пират", 5));
     }
 
 }
