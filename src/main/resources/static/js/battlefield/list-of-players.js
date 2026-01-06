3095ab4ebb5c4aded72253f9701dec41220666ae
TheSpace-hub <biometer2020@gmail.com>
2026-01-06 21:43:43 +0300

    Add list of players js logic

---
+++ src/main/resources/static/js/battlefield/list-of-players.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function () {
+    addPlayer('Злодей Крю')
+});
+
+/**
+ * Add new player into list.
+ * @param username Player's name.
+ */
+function addPlayer(username) {
+    document.querySelector('#list-of-players').insertAdjacentHTML('beforeend', createItem(username))
+}
+
+/**
+ * Create the HTML item.
+ * @param username Player's username
+ * @returns {string}
+ */
+function createItem(username) {
+    return `
+<li class="list-group-item d-flex justify-content-between align-items-center">
+    <div>
+        <span class="player-color-dot player-2-color"></span>
+        <span>${username}</span>
+    </div>
+    <div class="ship-count">Подключение...</div>
+</li>
+    `;
+}
