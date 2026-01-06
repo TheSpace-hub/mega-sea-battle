cba40d6bd795e30fccec90164a959598a9176f4e
TheSpace-hub <biometer2020@gmail.com>
2026-01-06 20:24:50 +0300

    Send info about games

---
+++ src/main/java/hub/thespace/megaseabattle/SenderOfPages.java
@@ -0,0 +1,23 @@
+package hub.thespace.megaseabattle;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+@Slf4j
+public class SenderOfPages {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        return "index";
+    }
+
+    @GetMapping("/game/{id}")
+    public String game(@PathVariable String id, Model model) {
+        return "battlefield";
+    }
+
+}
