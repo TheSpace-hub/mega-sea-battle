185fae88ac2011a5e1f523f880dd8c1377e57157
TheSpace-hub <biometer2020@gmail.com>
2026-01-07 21:34:53 +0300

    Add generate pubic info func

--- src/main/java/hub/thespace/megaseabattle/game/Game.java
+++ src/main/java/hub/thespace/megaseabattle/game/Game.java
@@ -6,10 +6,10 @@ import java.util.List;
 /**
  * An object of this class belongs to an instance of the game.
  */
-public record Game(String id, int maxPlayers, List<Player> players) {
+public record Game(String id, int maxPlayers, List<Player> players, List<Field.Position> openCells) {
 
     public Game(String id, int maxPlayers) {
-        this(id, maxPlayers, List.of());
+        this(id, maxPlayers, List.of(), List.of());
     }
 
     /**
@@ -20,6 +20,22 @@ public record Game(String id, int maxPlayers, List<Player> players) {
      */
     public void addPlayer(String username, Field field) {
         this.players.add(new Player(username, field));
+    }
+
+    /**
+     * Generate info for public. This method remove secret player's info.
+     * @return Public data.
+     */
+    public Game generatePublicInfo() {
+        Game game = new Game(id, maxPlayers, List.of(), openCells);
+        for (Player player : players) {
+            Field field = new Field();
+            for (Field.Position position : openCells) {
+                field.setCellState(position, player.getField().getCellState(position));
+            }
+            game.addPlayer(player.getUsername(), field);
+        }
+        return game;
     }
 
 }
