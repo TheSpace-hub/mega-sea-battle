f966bc5322e095f044e57606cc75456d6ef20ac5
TheSpace-hub <biometer2020@gmail.com>
2026-01-07 09:08:56 +0300

    Change status

---
+++ src/main/resources/static/js/battlefield/status.js
@@ -0,0 +1,34 @@
+export const gameStatusTypes = Object.freeze({
+    WAITING_SELF_START: 'waiting-self-start',
+    WAITING_OTHER_START: 'waiting-other-start',
+    WAITING_SELF_MOVE: 'waiting-self-move',
+    WAITING_OTHER_MOVE: 'waiting-other-move',
+    GAME_END: 'game-end',
+})
+
+/**
+ * Change game status.
+ * @param type Game status.
+ * @param username Player's name if needed. Can be null.
+ */
+export function changeGameStatus(type, username = null) {
+    const statusInfo = document.querySelector('#status-info')
+    const statusUsername = document.querySelector('#status-username')
+
+    if (type === 'waiting-self-start') {
+        statusInfo.textContent = 'Расставь свои корабли на поле, а затем начни игру.'
+        statusUsername.textContent = ''
+    } else if (type === 'waiting-other-start') {
+        statusInfo.textContent = 'Дождись, пока другие расставят свой флот.'
+        statusUsername.textContent = ''
+    } else if (type === 'waiting-self-move') {
+        statusInfo.textContent = 'Сделай выстрел, выбрав клетку на поле.'
+        statusUsername.textContent = username
+    } else if (type === 'waiting-other-move') {
+        statusInfo.textContent = 'Подожди, сейчас ход игрока '
+        statusUsername.textContent = username
+    } else if (type === 'waiting-other-move') {
+        statusInfo.textContent = 'Весь флот на дне! В игре победил '
+        statusUsername.textContent = username
+    }
+}
