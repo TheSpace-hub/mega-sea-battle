548812c3a52dd36fd6c31a76043608e1e8290df6
TheSpace-hub <biometer2020@gmail.com>
2026-01-09 13:02:32 +0300

    Remove logic

--- src/main/java/hub/thespace/megaseabattle/controllers/game/GameActionController.java
+++ src/main/java/hub/thespace/megaseabattle/controllers/game/GameActionController.java
@@ -3,13 +3,9 @@ package hub.thespace.megaseabattle.controllers.game;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
-import java.util.Map;
 
 @Controller
 @Slf4j
@@ -22,10 +18,4 @@ public class GameActionController {
         this.messagingTemplate = messagingTemplate;
     }
 
-    @MessageMapping("/game.register")
-    public Map<String, String> gameAddField(@Payload String username, @Payload String id, SimpMessageHeaderAccessor headerAccessor) {
-        log.info("Player {} is registered in game {} in session {}", username, id, headerAccessor.getSessionId());
-
-        return Map.of("status", "ok");
-    }
 }
